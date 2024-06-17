6eabde7e2f71dbaf9c0b7de1c40c00e720233ebb
franciscog98 <franciscomanuel.gonzalez3@gmail.com>
2024-06-17 17:27:20 -0300

    feat: buscador-de-lista caso 0

--- proyectos-live/buscador-de-lista/caso-0/src/App.tsx
+++ proyectos-live/buscador-de-lista/caso-0/src/App.tsx
@@ -9,21 +9,30 @@ function App() {
   const [query, setQuery] = useState<string>("");
 
   useEffect(() => {
-    api.search(query);
+    api.search(query)
+    .then(data => setProducts(data));
   }, [query]);
 
   return (
     <main>
       <h1>Tienda digitaloncy</h1>
-      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target)} />
+      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target.value)} />
       <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <h4>{product.title}</h4>
-            <p>{product.description}</p>
-            <span>$ {product.price}</span>
-          </li>
-        ))}
+        {
+          products.length > 0
+          ? (
+            products.map((product) => (
+              <li key={product.id}>
+                <h4>{product.title}</h4>
+                <p>{product.description}</p>
+                <span className={`${product.price <= 100 ? 'sale': ''}`}>$ {product.price}</span>
+              </li>
+            ))
+          )
+          : (
+            <span>cargando...</span>
+          )
+        }
       </ul>
     </main>
   );
